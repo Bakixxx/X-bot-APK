5279fdc53818a92b72eb6f12e83f6c9aca154e56
Bakixxx <abdulbakiturkmenoglu@gmail.com>
2025-04-10 20:41:20 +0300

    Update cmf.dart

--- lib/indicators/cmf.dart
+++ lib/indicators/cmf.dart
@@ -1,29 +1,38 @@
-class CMF {
-  double calculateCMF(
-    List<double> high,
-    List<double> low,
-    List<double> close,
-    List<double> volume,
-    int period,
-  ) {
+class ChaikinMoneyFlow {
+  /// CMF değeri
+  final double value;
+
+  ChaikinMoneyFlow({required this.value});
+
+  /// Chaikin Money Flow hesaplama
+  static ChaikinMoneyFlow calculate({
+    required List<double> high,
+    required List<double> low,
+    required List<double> close,
+    required List<double> volume,
+    int period = 20,
+  }) {
     if (high.length < period ||
         low.length < period ||
         close.length < period ||
         volume.length < period) {
-      throw ArgumentError('Veri sayısı, periyottan az olmamalı');
+      throw ArgumentError('Yetersiz veri: $period periyot için yeterli veri yok.');
     }
 
-    double moneyFlowVolumeSum = 0;
-    double volumeSum = 0;
+    double adlNumerator = 0;
+    double adlDenominator = 0;
 
     for (int i = high.length - period; i < high.length; i++) {
-      final moneyFlowMultiplier =
-          ((close[i] - low[i]) - (high[i] - close[i])) / (high[i] - low[i]);
+      final moneyFlowMultiplier = ((close[i] - low[i]) - (high[i] - close[i])) /
+          (high[i] - low[i] == 0 ? 1 : high[i] - low[i]);
+
       final moneyFlowVolume = moneyFlowMultiplier * volume[i];
-      moneyFlowVolumeSum += moneyFlowVolume;
-      volumeSum += volume[i];
+      adlNumerator += moneyFlowVolume;
+      adlDenominator += volume[i];
     }
 
-    return volumeSum != 0 ? moneyFlowVolumeSum / volumeSum : 0;
+    final cmf = adlNumerator / (adlDenominator == 0 ? 1 : adlDenominator);
+
+    return ChaikinMoneyFlow(value: cmf);
   }
 }
