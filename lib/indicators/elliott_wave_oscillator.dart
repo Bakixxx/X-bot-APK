2ced4d58c994a20432077fe6d2f3ba8c2c31d1c0
Bakixxx <abdulbakiturkmenoglu@gmail.com>
2025-04-10 20:28:33 +0300

    Update elliott_wave_oscillator.dart

--- lib/indicators/elliott_wave_oscillator.dart
+++ lib/indicators/elliott_wave_oscillator.dart
@@ -1,36 +1,11 @@
 class ElliottWaveOscillator {
-  List<double> calculateEWO(List<double> closePrices, {int fastPeriod = 5, int slowPeriod = 35}) {
-    if (closePrices.length < slowPeriod) {
-      throw ArgumentError('Yeterli veri yok. En az $slowPeriod kapanış fiyatı gerekiyor.');
-    }
-
+  List<double> calculate(List<double> closePrices) {
     List<double> ewo = [];
-    List<double> fastEMA = _calculateEMA(closePrices, fastPeriod);
-    List<double> slowEMA = _calculateEMA(closePrices, slowPeriod);
-
-    for (int i = 0; i < slowEMA.length; i++) {
-      ewo.add(fastEMA[i + (slowPeriod - fastPeriod)] - slowEMA[i]);
+    for (int i = 13; i < closePrices.length; i++) {
+      double shortEMA = closePrices.sublist(i - 5, i).reduce((a, b) => a + b) / 5;
+      double longEMA = closePrices.sublist(i - 35, i).reduce((a, b) => a + b) / 35;
+      ewo.add(shortEMA - longEMA);
     }
-
     return ewo;
   }
-
-  List<double> _calculateEMA(List<double> prices, int period) {
-    List<double> ema = [];
-    double multiplier = 2 / (period + 1);
-
-    double sum = 0;
-    for (int i = 0; i < period; i++) {
-      sum += prices[i];
-    }
-    double firstEMA = sum / period;
-    ema.add(firstEMA);
-
-    for (int i = period; i < prices.length; i++) {
-      double nextEMA = ((prices[i] - ema.last) * multiplier) + ema.last;
-      ema.add(nextEMA);
-    }
-
-    return ema;
-  }
-}
+}  
