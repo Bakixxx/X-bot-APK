28417206aa78363d01edd4dad2320652c32d3c25
Bakixxx <abdulbakiturkmenoglu@gmail.com>
2025-04-10 19:04:21 +0300

    Create app_constants.dart

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,10 @@
+class AppConstants {
+  static const String apiKeyLabel = 'API Anahtarı';
+  static const String secretKeyLabel = 'Gizli Anahtar';
+
+  static const String apiKeyHint = 'Binance API Anahtarınızı girin';
+  static const String secretKeyHint = 'Binance Gizli Anahtarınızı girin';
+
+  static const String saveButtonLabel = 'Kaydet';
+  static const String savedMessage = 'API Anahtarları kaydedildi!';
+}
