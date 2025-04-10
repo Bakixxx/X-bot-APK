14a9bcb20711e3c09989ae840a2d5459a8283119
Bakixxx <abdulbakiturkmenoglu@gmail.com>
2025-04-10 20:35:22 +0300

    Update cci.dart

--- lib/indicators/cci.dart
+++ lib/indicators/cci.dart
@@ -1,24 +1,40 @@
+import 'dart:math';
+
 class CCI {
-  double calculateCCI(List<double> high, List<double> low, List<double> close, int period) {
-    if (high.length < period || low.length < period || close.length < period) {
-      throw ArgumentError('Yetersiz veri: Tüm listeler en az $period veri içermelidir.');
+  /// CCI (Commodity Channel Index) hesaplama
+  /// typicalPrices: her mum için typical price (high + low + close) / 3
+  /// period: genelde 14 kullanılır
+  static List<double> calculate({
+    required List<double> highs,
+    required List<double> lows,
+    required List<double> closes,
+    int period = 14,
+  }) {
+    List<double> cciValues = [];
+
+    for (int i = 0; i < highs.length; i++) {
+      if (i + 1 < period) {
+        cciValues.add(0);
+        continue;
+      }
+
+      List<double> tpList = [];
+      for (int j = i + 1 - period; j <= i; j++) {
+        double tp = (highs[j] + lows[j] + closes[j]) / 3;
+        tpList.add(tp);
+      }
+
+      double typicalPrice = tpList.last;
+      double sma = tpList.reduce((a, b) => a + b) / period;
+      double meanDeviation = tpList
+              .map((tp) => (tp - sma).abs())
+              .reduce((a, b) => a + b) /
+          period;
+
+      double cci = meanDeviation == 0 ? 0 : (typicalPrice - sma) / (0.015 * meanDeviation);
+      cciValues.add(cci);
     }
 
-    List<double> typicalPrices = [];
-    for (int i = 0; i < period; i++) {
-      double tp = (high[i] + low[i] + close[i]) / 3;
-      typicalPrices.add(tp);
-    }
-
-    double sma = typicalPrices.reduce((a, b) => a + b) / period;
-
-    double meanDeviation = typicalPrices
-        .map((tp) => (tp - sma).abs())
-        .reduce((a, b) => a + b) / period;
-
-    double currentTP = typicalPrices.last;
-    double cci = (currentTP - sma) / (0.015 * meanDeviation);
-
-    return cci;
+    return cciValues;
   }
 }
