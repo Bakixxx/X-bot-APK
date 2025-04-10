bb374367e0493522eb6caf70ece31f1834940861
Bakixxx <abdulbakiturkmenoglu@gmail.com>
2025-04-10 20:30:14 +0300

    Update parabolic_sar.dart

--- lib/indicators/parabolic_sar.dart
+++ lib/indicators/parabolic_sar.dart
@@ -1,57 +1,44 @@
 class ParabolicSAR {
-  final double accelerationFactor;
-  final double maxAccelerationFactor;
-
-  ParabolicSAR({
-    this.accelerationFactor = 0.02,
-    this.maxAccelerationFactor = 0.2,
-  });
-
-  List<double?> calculate(List<double> highs, List<double> lows) {
-    List<double?> sar = List.filled(highs.length, null);
-    bool isUpTrend = true;
-    double af = accelerationFactor;
+  static List<double> calculate({
+    required List<double> highs,
+    required List<double> lows,
+    required double step,
+    required double maxStep,
+  }) {
+    List<double> sar = [];
+    double af = step;
+    bool isUptrend = true;
     double ep = highs[0];
-    double prevSar = lows[0];
+    double psar = lows[0];
 
     for (int i = 1; i < highs.length; i++) {
-      if (isUpTrend) {
-        if (highs[i] > ep) {
-          ep = highs[i];
-          af = (af + accelerationFactor).clamp(0, maxAccelerationFactor);
-        }
+      sar.add(psar);
 
-        double currentSar = prevSar + af * (ep - prevSar);
-        currentSar = currentSar > lows[i - 1] ? lows[i - 1] : currentSar;
-        sar[i] = currentSar;
-
-        if (lows[i] < currentSar) {
-          isUpTrend = false;
-          af = accelerationFactor;
+      if (isUptrend) {
+        psar = psar + af * (ep - psar);
+        if (lows[i] < psar) {
+          isUptrend = false;
+          psar = ep;
           ep = lows[i];
-          prevSar = highs[i];
-          sar[i] = prevSar;
+          af = step;
         } else {
-          prevSar = currentSar;
+          if (highs[i] > ep) {
+            ep = highs[i];
+            af = (af + step).clamp(0, maxStep);
+          }
         }
       } else {
-        if (lows[i] < ep) {
-          ep = lows[i];
-          af = (af + accelerationFactor).clamp(0, maxAccelerationFactor);
-        }
-
-        double currentSar = prevSar + af * (ep - prevSar);
-        currentSar = currentSar < highs[i - 1] ? highs[i - 1] : currentSar;
-        sar[i] = currentSar;
-
-        if (highs[i] > currentSar) {
-          isUpTrend = true;
-          af = accelerationFactor;
+        psar = psar + af * (ep - psar);
+        if (highs[i] > psar) {
+          isUptrend = true;
+          psar = ep;
           ep = highs[i];
-          prevSar = lows[i];
-          sar[i] = prevSar;
+          af = step;
         } else {
-          prevSar = currentSar;
+          if (lows[i] < ep) {
+            ep = lows[i];
+            af = (af + step).clamp(0, maxStep);
+          }
         }
       }
     }
