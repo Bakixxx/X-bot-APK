75dd92f635dc2cbf4c81341cdad82e4e4d91be8d
Bakixxx <abdulbakiturkmenoglu@gmail.com>
2025-04-10 20:33:47 +0300

    Update fibonacci_retracement.dart

--- lib/indicators/fibonacci_retracement.dart
+++ lib/indicators/fibonacci_retracement.dart
@@ -1,14 +1,18 @@
 class FibonacciRetracement {
-  Map<String, double> calculateLevels(double high, double low) {
-    final difference = high - low;
+  /// Giriş olarak bir zaman dilimindeki en yüksek ve en düşük fiyatı alır
+  /// ve geri çekilme seviyelerini döndürür.
+  static Map<String, double> calculate({
+    required double high,
+    required double low,
+  }) {
+    double diff = high - low;
+
     return {
-      '0.0%': high,
-      '23.6%': high - (difference * 0.236),
-      '38.2%': high - (difference * 0.382),
-      '50.0%': high - (difference * 0.500),
-      '61.8%': high - (difference * 0.618),
-      '78.6%': high - (difference * 0.786),
-      '100.0%': low,
+      '23.6%': high - (diff * 0.236),
+      '38.2%': high - (diff * 0.382),
+      '50.0%': high - (diff * 0.5),
+      '61.8%': high - (diff * 0.618),
+      '78.6%': high - (diff * 0.786),
     };
   }
 }
