8cb2e4e187b6ae4212ac8652b964fc11e13462b1
Bakixxx <abdulbakiturkmenoglu@gmail.com>
2025-04-10 20:36:51 +0300

    Update pivot_points.dart

--- lib/indicators/pivot_points.dart
+++ lib/indicators/pivot_points.dart
@@ -1,21 +1,45 @@
 class PivotPoints {
-  Map<String, double> calculatePivotPoints(double high, double low, double close) {
-    double pivot = (high + low + close) / 3;
-    double resistance1 = (2 * pivot) - low;
-    double support1 = (2 * pivot) - high;
-    double resistance2 = pivot + (high - low);
-    double support2 = pivot - (high - low);
-    double resistance3 = high + 2 * (pivot - low);
-    double support3 = low - 2 * (high - pivot);
+  final double pivot;
+  final double resistance1;
+  final double resistance2;
+  final double resistance3;
+  final double support1;
+  final double support2;
+  final double support3;
 
-    return {
-      'Pivot': pivot,
-      'R1': resistance1,
-      'S1': support1,
-      'R2': resistance2,
-      'S2': support2,
-      'R3': resistance3,
-      'S3': support3,
-    };
+  PivotPoints({
+    required this.pivot,
+    required this.resistance1,
+    required this.resistance2,
+    required this.resistance3,
+    required this.support1,
+    required this.support2,
+    required this.support3,
+  });
+
+  /// Pivot Noktaları Hesaplama (en son mum verilerine göre)
+  static PivotPoints calculate({
+    required double high,
+    required double low,
+    required double close,
+  }) {
+    final pivot = (high + low + close) / 3;
+    final r1 = 2 * pivot - low;
+    final r2 = pivot + (high - low);
+    final r3 = high + 2 * (pivot - low);
+
+    final s1 = 2 * pivot - high;
+    final s2 = pivot - (high - low);
+    final s3 = low - 2 * (high - pivot);
+
+    return PivotPoints(
+      pivot: pivot,
+      resistance1: r1,
+      resistance2: r2,
+      resistance3: r3,
+      support1: s1,
+      support2: s2,
+      support3: s3,
+    );
   }
 }
