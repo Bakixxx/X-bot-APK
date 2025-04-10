667b56242a4ed824296c1b341cea5aad077a9acc
Bakixxx <abdulbakiturkmenoglu@gmail.com>
2025-04-10 19:43:46 +0300

    Update binance_service.dart

--- lib/services/binance_service.dart
+++ lib/services/binance_service.dart
@@ -1,18 +1,51 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:crypto/crypto.dart';
 
 class BinanceService {
-  final String baseUrl = 'https://api.binance.com';
+  final String apiKey;
+  final String apiSecret;
 
-  Future<double?> fetchCurrentPrice(String symbol) async {
-    final response = await http.get(Uri.parse('$baseUrl/api/v3/ticker/price?symbol=$symbol'));
+  BinanceService({required this.apiKey, required this.apiSecret});
 
+  Future<String> getServerTime() async {
+    final response = await http.get(Uri.parse('https://api.binance.com/api/v3/time'));
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      return double.tryParse(data['price']);
+      return data['serverTime'].toString();
     } else {
-      print('Fiyat alınamadı: ${response.body}');
-      return null;
+      throw Exception('Server zamanını alamadı: ${response.body}');
     }
+  }
+
+  Future<String> getAccountInfo() async {
+    final timestamp = await getServerTime();
+    final query = 'timestamp=$timestamp';
+    final signature = _generateSignature(query);
+
+    final uri = Uri.parse('https://api.binance.com/api/v3/account?$query&signature=$signature');
+
+    final response = await http.get(
+      uri,
+      headers: {
+        'X-MBX-APIKEY': apiKey,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception('Hesap bilgisi alınamadı: ${response.body}');
+    }
+  }
+
+  String _generateSignature(String query) {
+    final key = utf8.encode(apiSecret);
+    final message = utf8.encode(query);
+
+    final hmac = Hmac(sha256, key);
+    final digest = hmac.convert(message);
+
+    return digest.toString();
   }
 }
